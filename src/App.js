f5667ac4fa00dceb1f64617ce8fb6e5500bd1b7c
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-12-12 17:18:38 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -34,7 +33,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+
   return { items: state.items }
 }
 
