c0494948134c224ba408c06c800481c805edc608
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-12-12 17:25:08 -0500

    Done.

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+class App extends Component {
+
+  handleOnClickItems() {
+    this.props.store.dispatch({
+      type: 'GET_COUNT_OF_ITEMS',
+    });
+  }
+
+  handleOnClickUsers() {
+    this.props.store.dispatch({
+      type: 'GET_COUNT_OF_USERS',
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <button onClick={() => this.handleOnClickItems()}>
+            Click to change items count
+            </button>
+          <button onClick={() => this.handleOnClickUsers()}>
+            Click to change user count
+          </button>
+          <p>{this.props.items.length}</p>
+          <p> I can't get this lab to submit :\ so we're just adding a lil change here :)</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+
+  return { items: state.items }
+}
+
+export default connect( state =>({items: state.items}))(App)
